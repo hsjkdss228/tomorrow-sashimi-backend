c285f08fec8ee67635a15f09890215a68b29f9af
Inwoo Hwang <hsjkdss228@naver.com>
2023-04-15 19:52:01 +0900

    프로젝트 세팅
    - Spring Boot 버전 3.0.5
    - 의존성 추가
      - Spring Boot DevTools
      - Spring Web
      - Spring Data JPA
      - Spring Security
      - Validation
      - H2 Database
      - PostgreSQL Driver

---
+++ src/main/java/com/inu/tomorrowsashimi/TomorrowSashimiApplication.java
@@ -0,0 +1,11 @@
+package com.inu.tomorrowsashimi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomorrowSashimiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TomorrowSashimiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/inu/tomorrowsashimi/TomorrowSashimiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.inu.tomorrowsashimi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomorrowSashimiApplicationTests {
+    @Test
+    void contextLoads() {
+
+    }
+}
