ef6f5bcf2896eff77ca34b04b8fb38acfcec760c
Inwoo Hwang <hsjkdss228@naver.com>
2023-04-25 02:09:21 +0900

    WebMvcConfigurer 재정의 후 주입
    모든 요청 경로, 모든 origin에 대해 GET, POST, PATCH, DELETE 요청 허용

---
+++ src/main/java/com/inu/tomorrowsashimi/config/WebMvcConfig.java
@@ -0,0 +1,29 @@
+package com.inu.tomorrowsashimi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name()
+                    );
+            }
+        };
+    }
+}
